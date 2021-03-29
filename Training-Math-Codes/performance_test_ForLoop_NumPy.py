c9bd0f44e7d2e1977e0766c15c6a58df4781134f
momchilantonov <eng.antonov@gmail.com>
2021-03-30 01:08:21 +0200

    Add some code

--- Training-Math-Codes/performance_test_ForLoop_NumPy.py
+++ Training-Math-Codes/performance_test_ForLoop_NumPy.py
@@ -26,26 +26,21 @@ def sum_array_with_np(array):
     return np.sum(array)
 
 
-def get_mean_times_with_list(exec_time_func, sizes, num_start=0, num_stop=100, trails=10):
-    mean_times = []
+def get_mean_times(sizes, num_start=0, num_stop=100, trails=10):
+    py_mean_times = []
+    np_mean_times = []
     for size in sizes:
-        array = list(np.random.uniform(num_start, num_stop, size=size))
-        current_times = []
+        np_array = np.random.uniform(num_start, num_stop, size=size)
+        py_list = list(np_array)
+        py_current_times = []
         for trail in range(trails):
-            current_times.append(exec_time_func(array))
-        mean_times.append(np.mean(current_times))
-    return mean_times
-
-
-def get_mean_times_with_array(exec_time_func, sizes, num_start=0, num_stop=100, trails=10):
-    mean_times = []
-    for size in sizes:
-        array = np.random.uniform(num_start, num_stop, size=size)
-        current_times = []
+            py_current_times.append(sum_array_with_py(py_list))
+        py_mean_times.append(np.mean(py_current_times))
+        np_current_times = []
         for trail in range(trails):
-            current_times.append(exec_time_func(array))
-        mean_times.append(np.mean(current_times))
-    return mean_times
+            np_current_times.append(sum_array_with_np(np_array))
+        np_mean_times.append(np.mean(np_current_times))
+    return py_mean_times, np_mean_times
 
 
 def get_speedup_factor(py_times, np_times):
@@ -58,43 +53,37 @@ def get_speedup_factor(py_times, np_times):
     return speedup
 
 
-def plot_times(sizes, py_times_ll, np_times_ll, py_times_ar, np_times_ar):
-    plt.plot(py_times_ll, color="red")
-    plt.plot(np_times_ll, color="blue")
-    plt.plot(py_times_ar, color="orange")
-    plt.plot(np_times_ar, color="green")
+def plot_times(sizes, py_times, np_times):
+    plt.plot(py_times, color="red")
+    plt.plot(np_times, color="blue")
     plt.xlim(0, len(sizes))
-    plt.ylim(-0.1, max(py_times_ar) * 1.05)
+    plt.ylim(-0.05, max(py_times) * 1.5)
     plt.xticks([0, 2, 4, 6, 8, 10], ["10²", "10³", "10⁴", "10⁵", "10⁶", "10⁷"], rotation=45)
     plt.yticks(rotation=45)
     plt.xlabel("array/list size", loc="center", fontsize="x-large")
     plt.ylabel("time[s]", loc="center", fontsize="x-large")
-    plt.legend(["PythonSum(list)", "NumPySum(list)", "PythonSum(array)", "NumPySum(array)"], fontsize="large",
-               loc='upper left')
+    plt.legend(["PythonSum(list)", "NumPySum(array)"], fontsize="large", loc='upper left')
     plt.title("Performance Test", fontsize="xx-large")
     plt.grid()
     plt.show()
 
 
 def plot_speedup_factor(sizes, speedup):
-    plt.plot(speedup, color="blue")
+    plt.plot(speedup, color="magenta")
     plt.xlim(0, len(sizes))
-    plt.ylim(0, max(speedup) * 1.05)
+    plt.ylim(0, max(speedup) * 1.1)
     plt.xticks([0, 2, 4, 6, 8, 10], ["10²", "10³", "10⁴", "10⁵", "10⁶", "10⁷"], rotation=45)
     plt.yticks(rotation=45)
     plt.xlabel("array/list size", loc="center", fontsize="x-large")
     plt.ylabel("speedup factor", loc="center", fontsize="x-large")
-    plt.legend(["NumPySumTimes(array)/PythonSum(list)"], fontsize="large", loc='upper left')
+    plt.legend(["NPTimes(array)/PYTimes(list)"], fontsize="large", loc='lower right')
     plt.title("SpeedUp Factor", fontsize="xx-large")
     plt.grid()
     plt.show()
 
 
 array_sizes = np.arange(10 ** 2, 10 ** 7, 10 ** 6)
-np_times_list = get_mean_times_with_list(sum_array_with_np, array_sizes)
-py_times_list = get_mean_times_with_list(sum_array_with_py, array_sizes)
-np_times_array = get_mean_times_with_array(sum_array_with_np, array_sizes)
-py_times_array = get_mean_times_with_array(sum_array_with_py, array_sizes)
+py_times_list, np_times_array = get_mean_times(array_sizes)
 speedup_factor = get_speedup_factor(py_times_list, np_times_array)
-plot_times(array_sizes, py_times_list, np_times_list, py_times_array, np_times_array)
+plot_times(array_sizes, py_times_list, np_times_array)
 plot_speedup_factor(array_sizes, speedup_factor)
