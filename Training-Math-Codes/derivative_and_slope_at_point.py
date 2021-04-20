6252946a5175e6824597022e3da75434a5bf8142
momchilantonov <eng.antonov@gmail.com>
2021-04-20 15:44:10 +0300

    Change some rows of code

--- Training-Math-Codes/derivative_and_slope_at_point.py
+++ Training-Math-Codes/derivative_and_slope_at_point.py
@@ -13,28 +13,29 @@ def calculate_derivative_at_point(function, point, precision=1e-7):
     return (function(point+precision)-function(point-precision)) / (2 * precision)
 
 
-def plot_derivative(function, derivative=None, min_x=-10, max_x=10):
-    vectorized_function = np.vectorize(function)
-    x = np.linspace(min_x, max_x, 1000)
-    y = vectorized_function(x)
-
-    if derivative is None:
-        dy = np.vectorize(calculate_derivative_at_point)(function, x)
-    else:
-        dy = np.vectorize(derivative)(x)
-
-    plt.plot(x, y, label="Function", color="blue")
-    plt.plot(x, dy, label="Derivative", color="red")
-    plt.xlabel("x", fontsize="x-large")
-    plt.ylabel("y", fontsize="x-large")
-    plt.legend(loc="lower right")
-    plt.title("Function and it's Derivative")
-    plt.grid()
-    plt.show()
-
-
-# plot_derivative(lambda x: x ** 2, lambda x: 2 * x)  # The derivative is calculated by hand
-# plot_derivative(lambda x: x ** 2)  # The derivative is not pre-calculated, should be calculated inside the function
+# def plot_derivative(function, derivative=None, min_x=-10, max_x=10):
+#     vectorized_function = np.vectorize(function)
+#     x = np.linspace(min_x, max_x, 1000)
+#     y = vectorized_function(x)
+#
+#     if derivative is None:
+#         dy = np.vectorize(calculate_derivative_at_point)(function, x)
+#     else:
+#         dy = np.vectorize(derivative)(x)
+#
+#     plt.plot(x, y, label="Function", color="blue")
+#     plt.plot(x, dy, label="Derivative", color="red")
+#     plt.xlabel("x", fontsize="x-large")
+#     plt.ylabel("y", fontsize="x-large")
+#     plt.legend(loc="lower right")
+#     plt.title("Function and its Derivative")
+#     plt.grid()
+#     plt.show()
+#
+#
+# plot_derivative(lambda x: x ** 2, lambda x: 2 * x) # The derivative is calculated by hand
+# plot_derivative(lambda x: x ** 2) # The derivative is not pre-calculated, should be calculated inside the function
+# plot_derivative(np.sin)
 
 
 def plot_derivative_at_point(function, point, derivative=None, min_x=-10, max_x=10):
@@ -52,20 +53,22 @@ def plot_derivative_at_point(function, point, derivative=None, min_x=-10, max_x=
     tangent_line_y = slope * tangent_line_x+intercept
 
     plt.plot(x, y, label="Function", color="blue")
-    plt.plot(tangent_line_x, tangent_line_y, label="Tangent Line", color="red")
+    plt.plot(tangent_line_x, tangent_line_y, label="Tangent", color="red")
+    plt.scatter(point, function(point), label="Contact point", color="green")
     plt.xlabel("x", fontsize="x-large")
     plt.ylabel("y", fontsize="x-large")
     plt.legend(loc="lower right")
-    plt.title("Function and it's Tangent Line")
+    plt.title("Function and its Tangent Line")
     plt.grid()
     plt.show()
 
 
-# plot_derivative_at_point(lambda x: x ** 2, 2)
-# for x in np.arange(-8, 10, 2):
-#     plot_derivative_at_point(lambda x: x ** 2, x)
-# for x in np.arange(-8, 10, 2):
-#     plot_derivative_at_point(np.sin, x)
+plot_derivative_at_point(lambda x: x ** 2, 2)
+plot_derivative_at_point(lambda x: x ** 2, 2, min_x = 0, max_x = 4)
+for x in np.arange(-8, 10, 2):
+    plot_derivative_at_point(lambda x: x ** 2, x)
+for x in np.arange(-8, 10, 2):
+    plot_derivative_at_point(np.sin, x)
 
 
 def animated_plot_derivative_at_point(function, point, derivative=None, min_x=-10, max_x=10):
@@ -87,15 +90,22 @@ def animated_plot_derivative_at_point(function, point, derivative=None, min_x=-1
     plt.xlabel("x", fontsize="x-large")
     plt.ylabel("y", fontsize="x-large")
     plt.legend(["Function", "Tangent Line"], loc="upper left")
-    plt.title("Function and it's Tangent Line")
+    plt.title("Function and its Tangent Line")
     plt.grid(True)
     camera.snap()
 
 
-fig = plt.figure(figsize=(5, 5), facecolor="w")
+fig = plt.figure(figsize=(5, 4), facecolor="w")
 camera = Camera(fig)
 for x in np.arange(-8, 10, 0.1):
     animated_plot_derivative_at_point(lambda x: x ** 2, x)
 animate = camera.animate()
+animate.save("derivative_1.gif")
 HTML(animate.to_html5_video())
-
+fig = plt.figure(figsize=(5, 4), facecolor="w")
+camera = Camera(fig)
+for x in np.arange(-8, 10, 0.1):
+    animated_plot_derivative_at_point(np.sin, x)
+animate = camera.animate()
+animate.save("derivative_2.gif")
+HTML(animate.to_html5_video())
